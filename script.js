3b1adc840e69a662e34d4171fc6d35fbf8a1b1f5
Sourabh10122002 <souravrawat444@gmail.com>
2023-07-18 18:44:07 +0530

    Toast Notification component completed

---
+++ script.js
@@ -0,0 +1,24 @@
+let toastBox = document.getElementById("toastBox");
+let successMsg = '<i class="fas fa-check-circle"></i> Successfully submitted!!';
+let errorMsg = '<i class="fas fa-times-circle red"></i> Please fix the error!!';
+let invalidMsg = '<i class="fas fa-exclamation-circle"></i> Invalid input, check again!!';
+
+function showToast(msg) {
+   let toast = document.createElement('div');
+   toast.classList.add('toast');
+   toast.innerHTML = msg;
+   toastBox.appendChild(toast);
+
+   if(msg.includes('error')){
+    toast.classList.add('error');
+   }
+   if(msg.includes('Invalid')){
+    toast.classList.add('invalid');
+   }
+
+   setTimeout(()=>{
+    toast.remove();
+   }, 3000);
+}
+
+
